fd478b37a5c56830b6ed12f58698efca6a3667bb
Seongnam Hong <hongdev26@gmail.com>
2026-01-15 18:40:16 +0900

    Feat : 스켈레톤 UI 적용 및 실데이터 요청 로직 작성

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,38 @@
 import ProductCard from "@/components/ProductCard";
 import isSoldout from "@/utils/isSoldout";
 import sortProducts from "@/utils/sortProduct";
-import { mockProducts } from "@/mocks/products";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ProductCardSkeleton from "@/components/ProductCardSkeleton";
+import { Product } from "@/types/product";
 
 const ITEMS_PER_PAGE = 4;
 
 export default function Home() {
-  const sortedProducts = sortProducts(mockProducts);
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(sortedProducts.length / ITEMS_PER_PAGE);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentProducts = sortedProducts.slice(startIndex, endIndex);
+  const currentProducts = products.slice(startIndex, endIndex);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch("https://api.zeri.pics");
+        const data = await res.json();
+        const sortedProducts = sortProducts(data.content);
+        setProducts(sortedProducts);
+      } catch (err) {
+        console.error("상품 데이터 조회 실패", err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchProducts();
+  }, [])
 
   return (
     <main className="mx-auto max-w-4xl px-4 py-8">
@@ -22,18 +42,23 @@ export default function Home() {
         척척밥상 주문서
       </h1>
       <section className="flex flex-col gap-4">
-        {currentProducts.map((product) => (
-          <ProductCard 
-            key={product.index}
-            index={product.index}
-            name={product.name}
-            price={product.price}
-            current={product.current}
-            limit={product.limit}
-            image={product.image}
-            isSoldout={isSoldout(product)}
-          />
-        ))}  
+        {loading
+          ? Array.from({ length: ITEMS_PER_PAGE }).map((_, i) => (
+            <ProductCardSkeleton key={i} />
+            ))
+          : currentProducts.map((product) => (
+            <ProductCard 
+              key={product.index}
+              index={product.index}
+              name={product.name}
+              price={product.price}
+              current={product.current}
+              limit={product.limit}
+              image={product.image}
+              isSoldout={isSoldout(product)}
+            />
+          ))
+        }  
       </section>
       <div className="mt-8 flex justify-center gap-2">
         <button
