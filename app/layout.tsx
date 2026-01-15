85bf1084fd6de67ae418b949dda0f9586fb46b64
Seongnam Hong <hongdev26@gmail.com>
2026-01-16 01:26:42 +0900

    Feat : metadata 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "척척밥상",
+  description: "척척밥상 주문서 과제 - 홍성남",
+};
 
 export default function RootLayout({
   children,
